d2d9dcde5c5f2d804efff47ce4c83488ba2cdac4
yipeng <alanxiaoyi@gmail.com>
2014-05-15 06:32:48 +0000

    major update - fully functional

--- xmpp_client.py
+++ xmpp_client.py
@@ -2,6 +2,9 @@ import sys
 import logging 
 import getpass
 import RPi.GPIO as GPIO
+import thread
+import time
+
 
 from optparse import OptionParser
 
@@ -23,42 +26,82 @@ class client_class(sleekxmpp.ClientXMPP):
 		self.msg=""
 		self.add_event_handler("session_start",self.start)
 		self.add_event_handler("message",self.receive)
-
 	def start(self,event):
+		GPIO.setmode(GPIO.BOARD)
 		self.send_presence()
 		self.get_roster()
-		GPIO.output(8,True)
 		
-#		self.send_message(mto=self.recipient, mbody=self.msg, mtype='type')
-#		self.disconnect(wait=True)
-
 
 	def mysend(self,recipient,msg):
-		if recipient != None:
+		if recipient != "":
 			self.recipient=recipient
-			
+		else:
+			print "you havn't specify recipient"
+			return
 
 		self.msg=msg;
-
 		self.send_message(mto=self.recipient,mbody=self.msg,mtype='chat')
 	
 
 	def receive(self, msg):
-
+		print "get somthing"
 		if msg['type'] in ('chat','normal'):
-			print "reply:"
-			print  msg['body']
+		
+			try:
+				if msg['body']=="forward":
+					thread.start_new_thread(run_motor,(0,0.3,))
+				elif msg['body']=="back":
+					thread.start_new_thread(run_motor,(1,0.3,))
+			except Exception, e:
+				print "ERROR: Cannot allocate thread"
+				print e
+#			xmpp.mysend(recipient,message)
+			print msg['from'],  " reply: ",msg['body']
 		return	
 
 	def disconnect(self):
 		self.disconnect(wait=True)
 
 
+def run_motor(direction, delay):
+	if direction==0:
+		
+		GPIO.output(8,True)
+		GPIO.output(12,False)
+		GPIO.output(16,True)
+		GPIO.output(15,False)
+		time.sleep(delay)
+		GPIO.output(8,False)
+		GPIO.output(12,False)
+		GPIO.output(16,False)
+		GPIO.output(15,False)
+	elif direction==1:
+		GPIO.output(12,True)
+		GPIO.output(8,False)
+		GPIO.output(15,True)
+		GPIO.output(16,False)
+		time.sleep(delay)
+		GPIO.output(12,False)
+		GPIO.output(8,False)
+		GPIO.output(15,False)
+		GPIO.output(16,False)	
+
+
+
+def get_user_list(xmpp):
+	global resp
+	resp=xmpp.get_roster()
+	for jid in resp['roster']['items']:
+		name=resp['roster']['items'][jid]['name']
+		print jid,"     ",name
+		
+	
+
 
 
 if __name__=='__main__':
 	optp=OptionParser()
-
+	recipient=""
 	
 	optp.add_option("-j", "--jid", dest="jid", help="jid to use")
 	optp.add_option("-p", "--password", dest="password", help="psw to use")
@@ -75,13 +118,17 @@ if __name__=='__main__':
 
 #	setup the GPIO port
 	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(8,GPIO.OUT,initial=GPIO.LOW)
+	GPIO.setup(8,GPIO.OUT)
+	GPIO.setup(12,GPIO.OUT)
+	GPIO.setup(15,GPIO.OUT)
+	GPIO.setup(16,GPIO.OUT)
 		
 
 	xmpp=client_class(opts.jid, opts.password)
 	xmpp.register_plugin('xep_0030')
+	xmpp.register_plugin('xep_0004')
+	xmpp.register_plugin('xep_0060')
 	xmpp.register_plugin('xep_0199')
-
 
 
 	if xmpp.connect(('talk.google.com', 5222)):
@@ -91,14 +138,47 @@ if __name__=='__main__':
 	else:
 		print("Unable to connect.")
 	
-	
-	recipient=raw_input("towhom?")
+
+	get_user_list(xmpp)
 	while True:
+		flag=0
 		message=raw_input()
+		messagelist=message.split(' ')
+		
+		if message=="ls":
+			get_user_list(xmpp)
+			continue
+
+		elif messagelist[0]=="to":
+			import re
+			matches=re.findall(r'\"(.+?)\"',message)
+			if matches == None:
+				matches[0]=""
+			print matches[0]
+			for jid in resp['roster']['items']:
+				print jid
+				if matches[0] == str(jid) or matches[0]==resp['roster']['items'][jid]['name']:
+					recipient=jid
+					flag=1
+					print "you are now chatting to  ",jid
+					break
+
+		if flag==1:
+			continue									
+
 		if message!="exit client":
+			try:
+				if message=="forward":
+					thread.start_new_thread(run_motor,(0,0.3,))
+				elif message=="back":
+					thread.start_new_thread(run_motor,(1,0.3,))
+			except Exception, e:
+				print "ERROR: Cannot allocate thread"
+				print e
 			xmpp.mysend(recipient,message)
 		else :
-			xmpp.disconnect		
+			xmpp.disconnect	
+			GPIO.cleanup()	
 			print("you disconnect!")
 			break
 	sys.exit(0)
